0be2ba08725f2bc6a57b0c1a41312384441a7a48
Juan <a23juanmg@iessanclemente.net>
2023-11-18 16:55:33 +0100

    Añadido script que calcula secuencia Fibonacci

---
+++ fibo/fibo.py
@@ -0,0 +1,14 @@
+def fibonacci(num): #defino funcion
+    num1 = 0   #asigno valores iniciales
+    num2 = 1
+    
+    for x in range (num):   #creo bucle for para calcular el numero
+        num3= num1 + num2
+        num1= num2
+        num2= num3         #este baile de variables calcula la sucesíon
+    
+    print(num1)             #imprimimos la variable que da el resultado
+    return num1
+
+entrada = int(input('Inserta un numero \n'))
+fibonacci(entrada)
